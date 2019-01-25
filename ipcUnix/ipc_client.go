84071a40381564429765805ebcda82228cb7d95a
yandong.yan <yandong.yan@upai.com>
2019-01-25 10:37:20 +0800

    fix: add ipc unix package

---
+++ ipcUnix/ipc_client.go
@@ -0,0 +1,74 @@
+package ipc
+
+import (
+	"net"
+	"io"
+	"time"
+	log "github.com/Sirupsen/logrus"
+
+)
+
+type IPCClient struct {
+	sockFile string
+	conn 	net.Conn
+}
+
+
+func reader(r io.Reader) string{
+	buf := make([]byte, 1024)
+	data := ""
+	// for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return data
+		}
+		log.Debugf("Client got:%v", string(buf[0:n]))
+		data = data + string(buf[0:n])
+	// }
+	return data
+}
+
+func writer(c io.Writer, msg string) error {
+	_,err := c.Write([]byte(msg))
+	if err != nil {
+		log.Errorf("write error:%v",err)
+		return err
+	}
+
+	return nil
+}
+
+func NewClient(sock string) *IPCClient {
+	instance := new(IPCClient)
+	instance.sockFile = sock
+
+	return instance
+}
+
+func (this *IPCClient) Write(msg string) error{
+	return writer(this.conn,msg)
+}
+
+func (this *IPCClient) Read() string{
+	return reader(this.conn)
+}
+
+func (this *IPCClient) Close() {
+
+	this.conn.Close()
+}
+
+
+func (this *IPCClient)CreateClient() error {
+	log.Debugf("create client begin!")
+	// ln,err := net.Dial("unix",this.sockFile)
+	timeout := time.Duration(5 * time.Second)
+	ln,err := net.DialTimeout("unix",this.sockFile,timeout)
+	if err != nil {
+		log.Fatalf("Listen error:%v",err)
+		return err
+	}
+	this.conn = ln
+
+	return nil
+}
---
+++ ipcUnix/ipc_server.go
@@ -0,0 +1,90 @@
+package ipcUnix
+
+import (
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	log "github.com/Sirupsen/logrus"
+
+)
+
+type CallbackFunc func(name string) string
+
+type IPCServer struct {
+	sockFile string
+	// 外部注册函数
+	Call CallbackFunc
+}
+
+
+func echoServer(this *IPCServer, c net.Conn) {
+	for {
+		buf := make([]byte, 512)
+		nr, err := c.Read(buf)
+		if err != nil {
+			return
+		}
+		data := string(buf[0:nr])
+		log.Debugf("Server got:", data)
+
+		msg := this.Call(data)
+		_, err = c.Write([]byte(msg))
+		if err != nil {
+			log.Errorf("Writing client error: %v", err)
+		}
+	}
+}
+
+func NewServer(sock string) *IPCServer {
+	instance := new(IPCServer)
+	instance.sockFile = sock
+	instance.Call = nil
+	return instance
+}
+
+func (this *IPCServer)RegsiterCallback(call CallbackFunc) error {
+	if this.Call != nil {
+		log.Errorf("cannot regsiter again!!!!")
+		return nil
+	}
+	this.Call = call
+	return nil
+}
+
+func (this *IPCServer)Close() error {
+	pid := syscall.Getppid()
+	log.Infof("main: Killing pid: %v", pid)
+	syscall.Kill(pid, syscall.SIGTERM)
+	return nil
+}
+
+func (this *IPCServer)CreateServer() error {
+	log.Debugf("create begin!")
+	ln,err := net.Listen("unix",this.sockFile)
+	if err != nil {
+		log.Fatalf("Listen error:%v",err)
+		return err
+	}
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+	go func(ln net.Listener, c chan os.Signal) {
+		sig := <-c
+		log.Infof("Caught signal %s: shutting down.", sig)
+		ln.Close()
+		os.Exit(0)
+	}(ln, sigc)
+
+	for {
+		fd, err := ln.Accept()
+		if err != nil {
+			log.Errorf("Accept error: ", err)
+			continue
+		}
+
+		go echoServer(this,fd)
+	}
+
+	return nil
+}
